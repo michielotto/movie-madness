e80c6eb17752f2d8046c5db2b905409ebc6a4fcc
MJ Otto <realmjotto@gmail.com>
2021-08-18 11:29:42 +0200

    additional frontend styling and google auth setup

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -25,20 +25,16 @@ const Form = ({ currentId, setCurrentId }) => {
       <h3>{currentId ? 'Edit this movie' : 'Create a movie'}</h3>
       <section>
         <div>
-          <h5>Title</h5>
-          <input placeholder="Guardians of the galaxy" value={postData.title} onChange={e => setPostData({ ...postData, title: e.target.value })} />
+          <input required placeholder="Movie Title" value={postData.title} onChange={e => setPostData({ ...postData, title: e.target.value })} />
         </div>
         <div>
-          <h5>Description</h5>
-          <textarea placeholder="This is an scifi action movie that plays off in space" value={postData.description} onChange={e => setPostData({ ...postData, description: e.target.value })} />
+          <textarea required placeholder="Give a description of the movie" value={postData.description} onChange={e => setPostData({ ...postData, description: e.target.value })} />
         </div>
         <div>
-          <h5>Year</h5>
-          <input type="number" placeholder="2015" value={postData.year} onChange={e => setPostData({ ...postData, year: e.target.value })} />
+          <input required type="number" placeholder="Year" value={postData.year} onChange={e => setPostData({ ...postData, year: e.target.value })} />
         </div>
         <div>
-          <h5>Duration</h5>
-          <input placeholder="2 hours 15 minutes" value={postData.duration} onChange={e => setPostData({ ...postData, duration: e.target.value })} />
+          <input required placeholder="Movie duration" value={postData.duration} onChange={e => setPostData({ ...postData, duration: e.target.value })} />
         </div>
         </section>
         <article>
@@ -47,12 +43,11 @@ const Form = ({ currentId, setCurrentId }) => {
             multiple={false} 
             onDone={({ base64 }) => setPostData({ ...postData, image: base64 })}
           />
-        </article>
-     
-        <div>
-          <button type="submit">Submit</button>
-          <span onClick={clear}>Clear</span>
+          <div>
+            <button type="submit">Submit</button>
+            <p onClick={clear}>Clear</p>
         </div>
+        </article>
     </form>
   )
 }
--- client/src/components/Title/Title.js
+++
@@ -1,4 +0,0 @@
-import React from 'react'
-import _ from './style.module.scss'
-
-export const Title = () => (<h1 className={_.Title}>Movie Madnesss</h1>)
